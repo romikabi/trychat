0e47db8fc50f951e42f458494179f814159b200b
romanabuzyarov <abuzyarovroman@gmail.com>
2017-04-16 03:59:11 +0300

    Return code properties and server response

--- src/server/Server.java
+++ src/server/Server.java
@@ -5,15 +5,14 @@ import chat.message.Message;
 import chat.user.Password;
 import chat.user.UserId;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -28,6 +27,7 @@ public class Server extends Thread {
     private ConcurrentMap<String, BlockingQueue<Message>> outgoing = null;
     private List<UserSocketManager> managers = null;
     private Reporter reporter = null;
+    private Properties codes;
 
     public void setupServer(Reporter reporter) throws Exception {
         //ServerSocket server = new ServerSocket(Integer.parseInt(args[1]));
@@ -37,6 +37,8 @@ public class Server extends Thread {
         managers = new ArrayList<>();
 
         this.reporter = reporter;
+        codes = new Properties();
+        codes.load(new FileInputStream(new File("data/codes.properties")));
 
         reporter.report("Setup is over.");
     }
@@ -58,61 +60,86 @@ public class Server extends Thread {
                 continue;
             }
             reporter.report("Connection accepted.");
-            handleIncomingConnection(connection);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    ENTERING code;
+                    do {
+                        code = handleIncomingConnection(connection);
+                    } while (code == ENTERING.REPEAT);
+                }
+            }).start();
         }
     }
 
-    private void handleIncomingConnection(Socket connection) {
-        BufferedReader reader = null;
+    private enum ENTERING {
+        SUCCESS,
+        REPEAT,
+        FAIL
+    }
+
+    private ENTERING handleIncomingConnection(Socket connection) {
+        BufferedReader readerUser = null;
         try {
-            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            readerUser = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         } catch (IOException e) {
-            return;
+            return ENTERING.FAIL;
+        }
+
+        PrintStream writerUser;
+        try {
+            writerUser = new PrintStream(connection.getOutputStream());
+        } catch (IOException e) {
+            return ENTERING.FAIL;
         }
 
         String content = null;
         try {
-            content = reader.readLine();
+            content = readerUser.readLine();
         } catch (IOException e) {
-            e.printStackTrace();
-            return;
+            return ENTERING.FAIL;
         }
         reporter.report("Got: \"" + content + "\".");
 
         String[] parts = content.split(" ");
         UserSocketManager man;
-        switch (parts[0]) {
-            // register
-            case "0":
-                try {
-                    users.register(new UserId(parts[1]), new Password(Integer.parseInt(parts[2])));
-                } catch (IllegalArgumentException e) {
-                    // todo Notify user about used nickname
-                    return;
-                }
-                reporter.report(parts[1] + " registered successfully.");
-                break;
-            //login
-            case "1":
-                try {
-                    man = new UserSocketManager(
-                            users.login(
-                                    new UserId(parts[1]),
-                                    new Password(Integer.parseInt(parts[2]))),
-                            connection,
-                            outgoing);
-                } catch (IllegalAccessException e) {
-                    // todo Notify user about wrong password
-                    return;
-                }
+        String code = parts[0];
 
-                managers.add(man);
-                outgoing.put(parts[1], new SynchronousQueue<>());
-                man.start();
-                reporter.report(parts[1] + " login successfully.");
-                break;
-            default:
-                break;
+        final String register = codes.getProperty("register_request");
+        final String login = codes.getProperty("login_request");
+        if (code.equals(register)) {
+            try {
+                users.register(new UserId(parts[1]), new Password(Integer.parseInt(parts[2])));
+            } catch (IllegalArgumentException e) {
+                writerUser.println(codes.getProperty("register_failure"));
+                return ENTERING.REPEAT;
+            }
+            reporter.report(parts[1] + " registered successfully.");
+            writerUser.println(codes.getProperty("register_success"));
+            return ENTERING.REPEAT;
+        } else if (code.equals(login)) {
+            //login
+            try {
+                man = new UserSocketManager(
+                        users.login(
+                                new UserId(parts[1]),
+                                new Password(Integer.parseInt(parts[2]))),
+                        connection,
+                        outgoing);
+            } catch (IllegalAccessException | IllegalArgumentException e) {
+                writerUser.println(codes.getProperty("login_failure"));
+                return ENTERING.REPEAT;
+            }
+
+            managers.add(man);
+            outgoing.put(parts[1], new SynchronousQueue<>());
+            man.start();
+            reporter.report(parts[1] + " login successfully.");
+            writerUser.println(codes.getProperty("login_success"));
+            return ENTERING.SUCCESS;
+        } else {
+            // todo notify about senseless message
+            return ENTERING.REPEAT;
         }
     }
 
